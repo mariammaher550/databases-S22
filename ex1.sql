db605c268ac89131ef581b49baa55bf418f45761
mariammaher550 <36730799+mariammaher550@users.noreply.github.com>
2022-04-04 23:36:16 +0300

    Update ex1.sql

--- ex1.sql
+++ ex1.sql
@@ -1,36 +1,66 @@
-CREATE TABLE item (
-  itemId INTEGER NOT NULL PRIMARY KEY,
-  itemName TEXT NOT NULL,
-  price float NOT NULL
-);
+-- create a table
 CREATE TABLE customer(
-    customerId INTEGER NOT NULL PRIMARY KEY,
+    customerId int NOT NULL primary key,
     customerName TEXT,
-    city TEXT,
-    orderId INTEGER NOT NULL
+    city TEXT
 );
 CREATE TABLE orders(
-    orderId INTEGER NOT NULL PRIMARY KEY,
-    date date,
-    quant INTEGER,
-    itemId INTEGER NOT NULL ,
-   FOREIGN KEY (itemId) references item(itemId)
+  orderId int NOT NULL primary key, 
+  customerId int,
+  date date, 
+  foreign key (customerId)
+  references customer (customerId)
+);
+CREATE TABLE item(
+    itemId int NOT NULL  primary key,
+    itemName TEXT,
+    price float
+);
+CREATE TABLE orderDetails(
+    orderId int,
+    itemId int,
+    quant int, 
+    foreign key (itemId) references item(itemId),
+    foreign key (orderId) references orders(orderId)
 );
 
--- Insereting values
-INSERT INTO item (itemId, itemName, price)
-VALUES (3786, "Net" , 35.0),
-        (4011, "Racket", 65.0),
+-- inserting data
+
+ INSERT INTO customer(customerId, customerName, city)
+ VALUES (101, "Martin", "Pargue"),
+        (107, "Herman", "Madrid"),
+        (110, "Pedro", "Moscow");
+        
+INSERT INTO item(itemId, itemName, price)
+VALUES (3786 , "Net", 35.0),
+        (4011, "Racket", 65.00),
         (9132, "Pack-3", 4.75),
         (5794, "Pack-6", 5.0),
-        (3171, "Cover", 10.0);
-INSERT INTO customer (customerId, customerName, city, orderId)
-VALUES (101, "Martin", "Prague" ,2301 ),
-        (107, "Herman", "Madrid", 2302),
-        (110, "Pedro", "Moscow", 2302);
-INSERT INTO orders (orderId, date, quant, itemId)        
-VALUES (2301, 23/02/2011, 3, 3786),
-        (2301, 23/02/2011, 6, 4011);
--- Queries
--- Calculate the total number of items per order 
--- and the total amount to pay for the order
+        (3141, "Cover", 10);
+        
+INSERT INTO orderDetails(orderId, itemId, quant)
+VALUES (2301, 3786, 3),
+        (2301, 4011, 6),
+        (2301, 9132, 8),
+        (2302, 5794, 4),
+        (2303, 4011, 2),
+        (2303, 3141, 2);
+        
+INSERT INTO orders(orderId, customerId, date)
+values (2301, 101, 23/02/2011),
+        (2302, 107, 25/02/2011),
+        (2303, 110 , 27/02/2011);
+        
+
+-- performing queries
+-- Calculate the total number of items per order and 
+-- the total amount to pay for the order
+SELECT orderId , SUM(quant), SUM(quant * price)
+    FROM orderDetails LEFT OUTER JOIN item
+    on orderDetails.itemId = item.itemId
+    GROUP by orderId;
+ 
+        
+--  Obtain the customer whose purchase in terms of money has been 
+-- greater than the others
+       
