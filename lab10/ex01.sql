2f9bb9f70d618dc1d5720576fe8b01c5894ef9aa
mariammaher550 <36730799+mariammaher550@users.noreply.github.com>
2022-04-22 16:05:24 +0300

    Update and rename New Text Document.txt to ex01.sql

---
+++ lab10/ex01.sql
@@ -0,0 +1,46 @@
+
+-- creating account table
+CREATE TABLE IF NOT EXISTS account (
+account_id int PRIMARY KEY NOT NULL,
+user_name String,
+credit int,
+currency char[3]
+);
+
+-- inserting 3 accounts 
+insert into account(account_id, user_name, credit, currency)
+values (1, 'One', 1000, 'RUB');
+
+insert into account(account_id, user_name, credit, currency)
+values (2, 'Account 2', 1000, 'RUB');
+
+insert into account(account_id, user_name, credit, currency)
+values (3, 'Account 3', 1000, 'RUB');
+
+BEGIN;
+--T1
+UPDATE account set credit = credit - 500
+	where account_id = 1;
+	
+UPDATE account set credit = credit + 500
+	where account_id = 3;
+savepoint T1;
+
+--T2
+UPDATE account set credit = credit - 700
+	where account_id = 2;
+	
+UPDATE account set credit = credit + 700
+	where account_id = 1;
+savepoint T2;
+
+--T3
+UPDATE account set credit = credit - 100
+	where account_id = 2;
+	
+UPDATE account set credit = credit + 100
+	where account_id = 3;
+savepoint T3;
+
+commit;
+
